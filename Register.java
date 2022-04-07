ef3f9fb9f811d66d874f540c24a3e4c9f517babb
Samed Uslu <uslusamed13@gmail.com>
2022-04-07 10:21:16 +0300

    Add Students
    Adding all students from database

--- Register.java
+++ Register.java
@@ -1,3 +1,40 @@
+<<<<<<< d3928b3d237f755b84fa64e623b22f48e2d4b5b4
 public class Register {
     
+=======
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Register {
+    public static void main (String[] args) {
+        ArrayList<Student> allStudents = addStudents();
+        
+
+    }
+
+    private static ArrayList<Student> addStudents() {
+        ArrayList<Student> result;
+        try{
+            File database = new File("Names_and_IDs.txt");
+            Scanner sc = new Scanner(database);
+            while(sc.hasNextLine()) {
+                String name = sc.next();
+                String surname = sc.next();
+                name += (" " + surname); 
+                String id = sc.next();
+                Student st = new Student (name, id);
+                result.add(st);
+            }
+        }
+        catch (FileNotFoundException err)  {
+            System.out.println("Error occured!");
+            err.printStackTrace();
+        }
+
+
+        return result;
+    }
+>>>>>>> Add Students
 }
