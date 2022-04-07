304c22d76cbf21b5234216a3d23d6aa6892ad5ce
Samed Uslu <uslusamed13@gmail.com>
2022-04-07 11:28:44 +0300

    Add Courses

--- Register.java
+++ Register.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Register {
     public static void main (String[] args) {
         ArrayList<Student> allStudents = addStudents();
-        
+        Courses allCourses = addCourses();
 
     }
 
@@ -22,6 +22,48 @@ public class Register {
                 String id = sc.next();
                 Student st = new Student (name, id);
                 result.add(st);
+            }
+        }
+        catch (FileNotFoundException err)  {
+            System.out.println("Error occured!");
+            err.printStackTrace();
+        }
+
+
+        return result;
+    }
+
+    private static Courses addCourses() {
+        Courses result = new Courses();
+        try{
+            File database = new File("Course_Database.txt");
+            Scanner sc = new Scanner(database);
+            int id = 1000;
+            while(sc.hasNextLine()) {
+                String name, section, insName, insSurname, insID;
+                Instructor instructor;
+                boolean[][] program = new boolean[6][9];
+                name = sc.next();
+                section = sc.next();
+                insName = sc.next();
+                insSurname = sc.next();
+                insName += (" " + insSurname);
+                insID = "" + id;
+                instructor = new Instructor (insName, insID);
+                for (int i = 0; i <= 6; i++) {
+                    for (int j = 0; j<= 9; j++) {
+                        int check = sc.nextInt();
+                        if(check == 1) {
+                            program [i][j] = true;
+                        }
+                        else {
+                            program [i][j] = false;
+                        }
+                    }
+                }
+                Course lesson = new Course (name, section, program, instructor);
+                result.addCourse(lesson);
+                id++;
             }
         }
         catch (FileNotFoundException err)  {
