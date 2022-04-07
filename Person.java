380068686e7c670ee4d4b44c9d56f2752f9cb7dc
Murat Çağrı Kara <cagri.kara@ug.bilkent.edu.tr>
2022-04-07 11:11:18 +0300

    Person up0

---
+++ Person.java
@@ -0,0 +1,52 @@
+public class Person {
+    protected String name;
+    protected int ID;
+    protected String[][] schedule2D;
+    protected final int rowNum = 9;
+    protected final int columnNum = 6; 
+    protected Courses courses = new Courses();
+    public Person(String name, int ID){
+        this.name = name;
+        this.ID = ID;
+        for(int i = 0; i < columnNum; i++){
+            for(int j = 0; j < rowNum; j++){
+                schedule2D[j][i] = null; 
+            }
+        } 
+    }
+    public String getName() {
+        return name;
+    }
+    public int getID() {
+        return ID;
+    }
+    public void addCourse(Course course){
+        if(this.doesOverlap(course)==false){
+            this.addCourseToProgram(course);
+        }
+        else{
+            System.out.println("The course spesified overlaps with another course.");
+        }
+    }
+    private void addCourseToProgram(Course course) {
+        for(int i = 0; i < columnNum; i++){
+            for(int j = 0; j < rowNum; j++){
+                if(course.getBooleanMatrix(j, i)){
+                    schedule2D[j][i]=course.getName();
+                }
+            }
+        } 
+    }
+    public boolean doesOverlap(Course course){
+        for(int i = 0; i < columnNum; i++){
+            for(int j = 0; j < rowNum; j++){
+                if(schedule2D[j][i]!=null){
+                    if(course.getBooleanMatrix(j, i)){
+                        return true;
+                    }
+                }
+            }
+        } 
+        return false;
+    }
+}
