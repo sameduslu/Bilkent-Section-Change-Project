060db6665a7bc959d2c49dbac27aa051213b5afe
Anfrian <gorkemkadir14@hotmail.com>
2022-05-07 16:37:00 +0300

    GUI SectionPage v1

--- Course.java
+++ Course.java
@@ -4,46 +4,38 @@ import java.util.ArrayList;
  * Course
  */
 public class Course {
-    String name;
-    String section;// sectionu string aldım
-    ArrayList<Student> students;
-    boolean[][] program;
-    Instructor instructor;
-    // Queue veri yapısı gerekecek;
-    static final int quota = 23;
+    private final String NAME;
+    private final String SECTION;
+    private final ArrayList<Student> students;
+    private final boolean[][] PROGRAM;
+    private final Instructor INSTRUCTOR;
+    
+    static final int quota = 25;
     /******************************************************************************************* */
     /**************************** CONSTRUCTOR ************************************************** */
     /******************************************************************************************* */
     public Course(String name, String section, boolean[][] matrice, Instructor instructor){// sonradan queue alacak
-        this.name = name;
-        this.section = section;
-        this.program = matrice;
-        this.instructor=instructor;
+        this.NAME = name;
+        this.SECTION = section;
+        this.PROGRAM = matrice;
+        this.INSTRUCTOR = instructor;
         students = new ArrayList<Student>();
     }
     /****************************************************************************************** */
     /**************************** METHODS ***************************************************** */
     /****************************************************************************************** */
-    public boolean addStudent(Student newStudent)
+    public void addStudent(Student newStudent)
     {
-        if(this.isThereQuota() && !newStudent.doesOverlap(this))
-        {
-            students.add(newStudent);
-            newStudent.addCourse(this);
-            return true;
-        }
-        else if (!this.isThereQuota()){
-            System.out.println("Quota is full, student cannot added to desired course!");
-            return false;
-        }
-        else {
-            System.out.println("This course overlapping with your current courses!");
-            return false;
-        }
-        
+        students.add(newStudent);
+        newStudent.addCourse(this);     
     }
-    public boolean isThereQuota()
-    {
+
+    public void removeStudent (Student removedStudent) {
+        students.remove(removedStudent);
+        removedStudent.removeCourse(this);
+    }
+
+    public boolean isThereQuota() {
         boolean areThereQuota = true;
         if(students.size()==quota)
         {
@@ -56,45 +48,47 @@ public class Course {
         }
         return areThereQuota;
     }
-    public int getAvailableQuota()
-    {
+
+    public int getAvailableQuota() {
         return quota-students.size();
     }
     //                            GET METHODS
-    public String getName()
-    {
-        return this.name;
+    public String getName() {
+        return this.NAME;
     }
-    public String getSection()
-    {
-        return this.section;
+
+    public String getSection() {
+        return this.SECTION;
     }
-    public ArrayList<Student> getStudents(){
+
+    public ArrayList<Student> getStudents() {
         return this.students;
     }
-    public boolean[][] getProgram(){
-        return this.program;
+
+    public boolean[][] getProgram() {
+        return this.PROGRAM;
     }
-    public Instructor getInstructor()
-    {
-        return this.instructor;
+
+    public Instructor getInstructor() {
+        return this.INSTRUCTOR;
     }
+
     public boolean doesOverlap (Course otherCourse) {
-        for (int i = 0; i < program.length; i++) {
-            for (int j = 0; j < program[0].length; j++) {
-                if(program[i][j] == true && otherCourse.getProgram()[i][j] == true) {
+        for (int i = 0; i < PROGRAM.length; i++) {
+            for (int j = 0; j < PROGRAM[j].length; j++) {
+                if(PROGRAM[i][j] == true && otherCourse.getProgram()[i][j] == true) {
                     return true;
                 }
             }
         }
         return false;
-    }kk + l
+    }
 
     public void printProgram() {
-        for (int i = 1; i < program.length; i++) {
-            for (int j = 0; j < program[0].length; j++) {
-                if(program[i][j]) {
-                    System.out.print("1");
+        for (int i = 0; i < PROGRAM.length; i++) {
+            for (int j = 0; j < PROGRAM[0].length; j++) {
+                if(PROGRAM[i][j]) {
+                    System.out.print("1 ");
                 }
                 else {
                     System.out.print("0 ");
@@ -106,9 +100,8 @@ public class Course {
 
     public String toString () {
         String result = "";
-        result += (this.name + " ") ;
-        result += (this.section);
+        result += (this.NAME + " ") ;
+        result += (this.SECTION);
         return result;
     }
-
 }
--- JavaApplication1/src/main/Person.java
+++ JavaApplication1/src/main/Person.java
@@ -1,14 +1,13 @@
 package main;
-
 import java.util.Arrays;
-public class Person {
-    protected String name;
-    protected String ID;
+public abstract class Person {
+
+    protected String name, ID;
     protected Course[][] schedule2D = new Course[rowNum][columnNum];
     protected boolean[][] program = new boolean[rowNum][columnNum]; 
-    protected static final int rowNum = 9;
-    protected static final int columnNum = 6;
+    protected static final int rowNum = 9, columnNum = 6;
     protected Courses courses = new Courses();
+
     public Person(String name, String ID){
         this.name = name;
         this.ID = ID;
@@ -26,26 +25,20 @@ public class Person {
             return schedule2D[row][column].getName() + "";
         }
     }
-    
-    public Course[][] getSchedule(){
-        return schedule2D;
-    }
-    
+
     public String getName() {
         return name;
     }
+
     public String getID() {
         return ID;
     }
-    public void addCourse(Course course){
-        if(this.doesOverlap(course)==false){
-            this.addCourseToProgram(course);
-        }
-        else{
-            System.out.println("The course specified overlaps with another course.");
-        }
+
+    public Courses getCourses () {
+        return courses;
     }
-    private void addCourseToProgram(Course course) {
+
+    public void addCourse (Course course){
         for(int i = 0; i < rowNum; i++){
             for(int j = 0; j < columnNum; j++){
                 if(course.getProgram()[i][j]){
@@ -54,17 +47,63 @@ public class Person {
                 }
             }
         } 
+        courses.addCourse(course);
     }
-    public boolean doesOverlap(Course course){
-        for(int i = 0; i < rowNum; i++){
+
+    public boolean doesOverlap (Course course){
+        for (int i = 0; i < courses.getSize(); i++) {
+            if (course.equals(courses.getCourse(i))) {
+                return true;
+            }
+        }
+        for (int i = 0; i < rowNum; i++){
             for(int j = 0; j < columnNum; j++){
-                if(this.program[i][j] && course.getProgram()[i][j]){
+                if (this.program[i][j] && this.schedule2D[i][j].getName().equals(course.getName())) {
+                    continue;
+                }
+                if (this.program[i][j] && course.getProgram()[i][j]){
                     return true;
                 }
             }
         } 
         return false;
     }
+
+    public boolean doesOverlap (Courses requestingCourses){
+        Courses noIncluded = new Courses();
+        //finding the courses which is not in the requesting courses and adding them to noIncluded 
+        for (int i = 0; i < courses.getSize(); i++) {
+            boolean ctr = true;
+            for (int j = 0; j < requestingCourses.getSize(); j++) {
+                if (requestingCourses.getCourse(j).getName().equals(courses.getCourse(i).getName())) {
+                    ctr = false;
+                }
+            }
+            if (ctr) {
+                noIncluded.addCourse(courses.getCourse(i));
+            }
+        }
+        
+        //checking the compatibility of requesting courses and no included courses
+        for (int i = 0; i < requestingCourses.getSize(); i++) {
+            for (int j = 0; j < noIncluded.getSize(); j++) {
+                if(requestingCourses.getCourse(i).doesOverlap(noIncluded.getCourse(j))){
+                    return true;
+                }
+            } 
+        }
+        //checking the compatibility of requesting courses with each other
+        for (int i = 0; i < requestingCourses.getSize(); i++) {
+            for (int j = i+1; j < requestingCourses.getSize(); j++) {
+                if(requestingCourses.getCourse(i).doesOverlap(requestingCourses.getCourse(j))){
+                    return true;
+                }
+            } 
+        }
+
+        return false;
+    }
+    
 
     public void printSchedule() {
         for (int i = 0; i < rowNum; i++) {
