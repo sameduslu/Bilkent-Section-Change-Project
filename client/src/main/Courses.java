f7c3d002d0f9aa7102ab8d9d22a2c84ba9ba6f93
Burhan Tabak <burhan.tabak@ug.bilkent.edu.tr>
2022-05-10 02:07:12 +0300

    back to normal

---
+++ client/src/main/Courses.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.util.ArrayList;
+/**
+ * set of Course objects
+ * 
+ * Last Update: 7.04.22
+ */
+public class Courses {
+    ArrayList<Course> courses;
+    // default constructor
+    public Courses(){
+        courses = new ArrayList<Course>(); 
+    }
+    /*************************************************************************************** */
+    /**************************** METHODS ************************************************** */
+    /*************************************************************************************** */
+    public void addCourse (Course newCourse)
+    {
+        courses.add(newCourse);
+    }
+
+    public void removeCourse (Course removedCourse) {
+        courses.remove(removedCourse);
+    }
+    
+    //Get methods
+    public ArrayList<Course> getCourses() {
+        return this.courses;
+    }
+
+    public int getSize() {
+        return courses.size();
+    }
+
+    public Course getCourse (int index) {
+        return courses.get(index);
+    }
+}
