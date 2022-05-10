31a501c74bd70f7e6f6ab44fae30d1c9ad4a0c3a
Burhan Tabak <burhan.tabak@ug.bilkent.edu.tr>
2022-05-10 13:31:30 +0300

    server auth

--- server/src/main/java/tr/edu/bilkent/cs/cs102/registerplusplus/server/controller/StudentController.java
+++ server/src/main/java/tr/edu/bilkent/cs/cs102/registerplusplus/server/controller/StudentController.java
@@ -1,20 +1,23 @@
 package tr.edu.bilkent.cs.cs102.registerplusplus.server.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tr.edu.bilkent.cs.cs102.registerplusplus.server.entity.Course;
 import tr.edu.bilkent.cs.cs102.registerplusplus.server.entity.Student;
+import tr.edu.bilkent.cs.cs102.registerplusplus.server.repo.CourseRepository;
 import tr.edu.bilkent.cs.cs102.registerplusplus.server.repo.StudentRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
-    private final StudentRepository repository;
+    private final StudentRepository studentRepository;
 
-    StudentController(StudentRepository repository) {
-        this.repository = repository;
+    private final CourseRepository courseRepository;
+
+    StudentController(StudentRepository repository, CourseRepository courseRepository) {
+        this.studentRepository = repository;
+        this.courseRepository = courseRepository;
     }
 
 
@@ -22,12 +25,35 @@ public class StudentController {
     // tag::get-aggregate-root[]
     @GetMapping("/students")
     public List<Student> all() {
-        return repository.findAll();
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/student/{id}")
+    public Student getStudentById(@PathVariable String id){
+        Optional<Student> studentById = studentRepository.findById(id);
+        if (studentById.isEmpty()){
+            return null;
+        }
+        Student student = studentById.get();
+        List<Course> coursesOfStudent = courseRepository.findCourseByStudentsId(id);
+        Course[][] schedule = student.getSchedule();
+        for(Course c : coursesOfStudent){
+            boolean[][] courseSchedule = c.getProgram();
+            for (int i = 0; i < courseSchedule.length; i++) {
+                for (int j = 0; j < courseSchedule[i].length; j++) {
+                    if (courseSchedule[i][j]){
+                        schedule[i][j] = c;
+                    }
+                }
+            }
+        }
+        student.setSchedule(schedule);
+        return student;
     }
     // end::get-aggregate-root[]
 
     @PostMapping("/student")
     public Student newItem(@RequestBody Student student) {
-        return repository.save(student);
+        return studentRepository.save(student);
     }
 }
--- server/src/main/java/tr/edu/bilkent/cs/cs102/registerplusplus/server/repo/DatabaseInitializer.java
+++ server/src/main/java/tr/edu/bilkent/cs/cs102/registerplusplus/server/repo/DatabaseInitializer.java
@@ -2,35 +2,37 @@ package tr.edu.bilkent.cs.cs102.registerplusplus.server.repo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tr.edu.bilkent.cs.cs102.registerplusplus.server.entity.Credentials;
+import tr.edu.bilkent.cs.cs102.registerplusplus.server.entity.Student;
+
+import java.io.File;
+import java.util.Scanner;
 
 @Configuration
 public class DatabaseInitializer {
 
     private static final Logger log = LoggerFactory.getLogger(DatabaseInitializer.class);
 
-//    @Bean
-//    CommandLineRunner itemRepositoryInit(ItemRepository repository) {
-//
-//        return args -> {
-//            System.out.println("Data creation started...");
-//            repository.save(...);
-//            System.out.println("Data creation complete...");
-//        };
-//    }
-//
-//    @Bean
-//    CommandLineRunner courseRepoInit(CourseRepository repo){
-//        return args -> {
-//            Course c = new Course();
-//            c.setId("CS102-2");
-//            c.setName("CS102");
-//            c.setSection("2");
-//            boolean[][] b = new boolean[9][6];
-//            b[1][2] = true;
-//            b[3][5] = true;
-//            c.setProgram(b);
-//            repo.save(c);
-//        };
-//    }
+    @Bean
+    CommandLineRunner CredentialsInit(CredentialsRepository credentialsRepository, StudentRepository studentRepository) {
+        return args -> {
+            Scanner in = new Scanner(new File("D:\\cs102\\Bilkent-Section-Change-Project\\client\\src\\LoginPage\\Names_and_IDs_Passwords.txt"));
+            while (in.hasNextLine()) {
+                String[] s = in.nextLine().split(" ");
+                Credentials c = new Credentials();
+                c.setUsername(s[s.length - 2]);
+                c.setPassword(s[s.length - 1]);
+                credentialsRepository.save(c);
+                String name = "";
+                for (int i = 0; i < s.length - 2; i++) {
+                    name += s[i] + ' ';
+                }
+                name = name.strip();
+                studentRepository.save(new Student(name, s[s.length - 2]));
+            }
+        };
+    }
 }
