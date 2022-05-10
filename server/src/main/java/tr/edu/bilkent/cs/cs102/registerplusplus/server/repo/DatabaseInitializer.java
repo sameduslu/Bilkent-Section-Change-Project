b5dd0e7ef85ffcbfbc2dc7a4d90a1d65e234eb30
Samed Uslu <uslusamed13@gmail.com>
2022-05-10 15:23:24 +0300

    Adding courses to the database

--- server/src/main/java/tr/edu/bilkent/cs/cs102/registerplusplus/server/repo/DatabaseInitializer.java
+++ server/src/main/java/tr/edu/bilkent/cs/cs102/registerplusplus/server/repo/DatabaseInitializer.java
@@ -5,11 +5,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import tr.edu.bilkent.cs.cs102.registerplusplus.server.entity.Course;
 import tr.edu.bilkent.cs.cs102.registerplusplus.server.entity.Credentials;
+import tr.edu.bilkent.cs.cs102.registerplusplus.server.entity.Instructor;
+import tr.edu.bilkent.cs.cs102.registerplusplus.server.entity.Person;
 import tr.edu.bilkent.cs.cs102.registerplusplus.server.entity.Student;
 
 import java.io.File;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 @Configuration
 public class DatabaseInitializer {
@@ -35,4 +40,53 @@ public class DatabaseInitializer {
             }
         };
     }
+
+    @Bean
+    CommandLineRunner addCourses(CourseRepository courseRepository) {
+        return args -> {    
+            try{
+                File database = new File("Course_Database.txt");
+                Scanner sc = new Scanner(database);
+                int id = 1000;
+                while(sc.hasNextLine()) {
+                    String name, section, insName, insSurname, insID;
+                    Instructor instructor;
+                    boolean[][] program = new boolean[Person.rowNum][Person.columnNum];
+                    name = sc.next();
+                    section = sc.next();
+                    insName = sc.next();
+                    insSurname = sc.next();
+                    insName += (" " + insSurname);
+                    insID = "" + id;
+                    instructor = new Instructor (insName, insID);
+                    for (int i = 0; i < Person.rowNum; i++) {
+                        for (int j = 0; j< Person.columnNum; j++) {
+                            int check = sc.nextInt();
+                            if(check == 1) {
+                                program [i][j] = true;
+                            }
+                            else {
+                                program [i][j] = false;
+                            }
+                        }
+                    }
+                    //Course lesson = new Course (name, section, program, instructor);
+                    Course lesson = new Course();
+                    lesson.setName(name);
+                    String id2 = name + "-" + section;
+                    lesson.setId(id2);
+                    lesson.setProgram(program);
+                    lesson.setInstructor(instructor);
+                    //instructor.addCourse(lesson); TODO
+                    courseRepository.save(lesson);
+                    id++;
+                }
+            }
+            catch (FileNotFoundException err)  {
+                System.out.println("Error occured!");
+                err.printStackTrace();
+            }
+        };
+    }
+
 }
